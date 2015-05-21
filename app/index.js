29c910ce3d175292329cd34c08c5ac2b5ca48da6
vaenow <luowenbiz@gmail.com>
2015-05-21 23:41:03 +0800

    add bootstrap, seajs, angularjs

---
+++ app/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by LUOWEN on 2015-05-21.
+ */
+// seajs 的简单配置
+seajs.config({
+    base: ".",
+    alias: {
+        "fairy": "assets/role_main_fairy.json"
+    },
+    // 文件编码
+    charset: 'utf-8',
+    // 调试模式
+    debug: true
+});
+
+//初始化全局变量
+window.game = {};
+
+// 加载入口模块
+seajs.use("./app/main");
